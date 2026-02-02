632be526c5ae6ce1652b314711fabf5b69b579e7
Musadique Muslim <musadiquemuslim868@gmail.com>
2026-02-01 19:04:36 -0700

    add

--- app.js
+++ app.js
@@ -1,58 +1,25 @@
-// Set Eid date (example: 31 March 2026)
-// const eidDate = new Date("March 20, 2026 00:00:00").getTime();
 
-// setInterval(() => {
-//   const now = new Date().getTime();
-//   let diff = eidDate - now;
-
-//   if (diff < 0) return;
-
-//   const seconds = Math.floor((diff / 1000) % 60);
-//   const minutes = Math.floor((diff / (1000 * 60)) % 60);
-//   const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
-
-//   const totalDays = Math.floor(diff / (1000 * 60 * 60 * 24));
-//   const months = Math.floor(totalDays / 30);
-//   const days = totalDays % 30;
-
-//   document.getElementById("months").innerText = months;
-//   document.getElementById("days").innerText = days;
-//   document.getElementById("hours").innerText = hours;
-//   document.getElementById("minutes").innerText = minutes;
-//   document.getElementById("seconds").innerText = seconds;
-// }, 1000);
 
 let eidDate = new Date("March 20, 2026 18:20:00").getTime();
-
 setInterval(() => {
-    let now = new Date().getTime();
-    let diff = eidDate - now;
     let day = document.getElementById("days");
     let hour = document.getElementById("hours");
     let minute = document.getElementById("minutes");
     let second = document.getElementById("seconds");
 
+    let now = new Date().getTime();
+    let diff = eidDate - now
+    console.log(diff);
+
     let d = Math.floor(diff / (1000 * 60 * 60 * 24));
-
-    let h = Math.floor(
-        (diff / (1000 * 60 * 60)) % 24
-    );
-
-    let m = Math.floor(
-        (diff / (1000 * 60)) % 60
-    );
-
-    let s = Math.floor(
-        (diff / 1000) % 60
-    );
-
-
-
-
+    let h =Math.floor(diff/(1000*60*60)%24);
+    let m = Math.floor((diff / (1000 * 60)) % 60);
+    let s = Math.floor((diff / 1000) % 60);
 
 
     day.innerHTML = d;
     hour.innerHTML = h;
     minute.innerHTML = m;
     second.innerHTML = s;
+
 })
